0eee2a17db19d9eaf20d7dbaf66dbce857c96386
Anupriyo Mandal <121713895+anupriyom@users.noreply.github.com>
2022-12-31 17:48:54 +0530

    Update collate-conjecture(3n+1).js

--- collate-conjecture(3n+1).js
+++ collate-conjecture(3n+1).js
@@ -1,17 +1,1 @@
-var hotpo = function(n){
-    let count = 0
-    while(true){
-      if (n == 1){
-        return count
-      }
-      if (n%2 == 0){
-        n = n / 2
-      }
-      else {
-        n = 3 * n + 1
-      }
-      count += 1
-      
-    }
-    
-}
+const hotpo = (n) => n == 1 ? 0 : 1 + hotpo(n % 2 ? 3 * n + 1: n / 2)
